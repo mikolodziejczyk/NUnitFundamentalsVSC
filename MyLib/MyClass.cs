32dfbb1a6af2b09b1e2fa6b8982bbf3e89cb1953
Michał Kołodziejczyk <mikolodziejczyk77@gmail.com>
2021-12-16 13:50:14 +0100

    initial

---
+++ MyLib/MyClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    /// <summary>
+    /// The class to perform tests on.
+    /// </summary>
+    public class MyClass
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public decimal Divide(decimal a, decimal b)
+        {
+            if (b == 0) throw new ArgumentException("The b argument cannot be null", "b");
+            return a / b;
+        }
+    }
+}
